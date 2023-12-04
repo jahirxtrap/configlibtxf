fff83b77d0431c38b7da3c3009ccc1aa98027b86
jahirxtrap <jahirroman2002@gmail.com>
2023-12-04 15:16:09 -0500

    Fixes

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 Config library for minecraft mods
 
+```java
+// initialize config
+TXFConfig.init(MODID, MyModConfig.class);
+
+// Registering config screens (Neo/Forge)
+ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
+        new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfig.getScreen(parent, MODID)));
+```
+
 forked from <a href="https://github.com/TeamMidnightDust/MidnightLib" target="_blank">TeamMidnightDust/MidnightLib</a>
--- forge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
+++ forge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
@@ -1,33 +1,17 @@
 package com.jahirtrap.configlib;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.ConfigGuiHandler;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import static net.minecraftforge.network.NetworkConstants.IGNORESERVERONLY;
 
 @Mod(ConfigLibMod.MODID)
-@Mod.EventBusSubscriber(modid = ConfigLibMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ConfigLibMod {
 
     public static final String MODID = "configlibtxf";
 
     public ConfigLibMod() {
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IGNORESERVERONLY, (remote, server) -> true));
-    }
-
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        ModList.get().forEachModContainer((modid, modContainer) -> {
-            if (TXFConfig.configClass.containsKey(modid)) {
-                modContainer.registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () ->
-                        new ConfigGuiHandler.ConfigGuiFactory((client, parent) -> TXFConfig.getScreen(parent, modid)));
-            }
-        });
     }
 }
