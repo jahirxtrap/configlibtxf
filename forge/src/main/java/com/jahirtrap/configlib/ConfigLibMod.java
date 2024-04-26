07408b6f2f232db49f52193dcb1e2d74668cacb0
jahirxtrap <jahirroman2002@gmail.com>
2024-04-25 19:22:00 -0500

    Fixes

--- forge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
+++ forge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
@@ -1,17 +1,10 @@
 package com.jahirtrap.configlib;
 
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-
-import static net.minecraftforge.fml.IExtensionPoint.DisplayTest.IGNORESERVERONLY;
 
 @Mod(ConfigLibMod.MODID)
 public class ConfigLibMod {
 
     public static final String MODID = "configlibtxf";
 
-    public ConfigLibMod() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IGNORESERVERONLY, (remote, server) -> true));
-    }
 }
