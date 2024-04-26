fbf5482eda9af3cc8b0ecaa49d2eab9ee86f9082
jahirxtrap <jahirroman2002@gmail.com>
2024-04-25 19:25:10 -0500

    1.20.5

--- neoforge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
+++ neoforge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
@@ -1,17 +1,10 @@
 package com.jahirtrap.configlib;
 
-import net.neoforged.fml.IExtensionPoint;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-
-import static net.neoforged.fml.IExtensionPoint.DisplayTest.IGNORESERVERONLY;
 
 @Mod(ConfigLibMod.MODID)
 public class ConfigLibMod {
 
     public static final String MODID = "configlibtxf";
 
-    public ConfigLibMod() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IGNORESERVERONLY, (remote, server) -> true));
-    }
 }
