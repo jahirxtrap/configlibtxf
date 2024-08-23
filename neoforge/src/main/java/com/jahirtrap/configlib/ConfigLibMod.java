e9fbe251f4f61dd6538fecea336b5cd0b5143863
jahirxtrap <jahirroman2002@gmail.com>
2024-08-23 07:57:29 -0500

    Config screen registry

--- neoforge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
+++ neoforge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
@@ -1,10 +1,25 @@
 package com.jahirtrap.configlib;
 
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 
 @Mod(ConfigLibMod.MODID)
+@Mod.EventBusSubscriber(modid = ConfigLibMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ConfigLibMod {
 
     public static final String MODID = "configlibtxf";
 
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        ModList.get().forEachModContainer((modid, container) -> {
+            if (TXFConfig.configClass.containsKey(modid)) {
+                container.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
+                        new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfig.getScreen(parent, modid)));
+            }
+        });
+    }
 }
