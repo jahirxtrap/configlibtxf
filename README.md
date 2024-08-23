85d0be42eff7a1c5f8457676ca3cc5b9c4a8f638
jahirxtrap <jahirroman2002@gmail.com>
2024-08-23 08:02:26 -0500

    Config screen registry

--- README.md
+++ README.md
@@ -4,15 +4,7 @@ Config library for minecraft mods
 
 ```java
 // initialize config
-TXFConfig.init(MODID, MyModConfig.class);
-
-// Registering config screens (Forge)
-ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-        new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfigClient.getScreen(parent, MODID)));
-
-// Registering config screens (NeoForge)
-ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () ->
-                (client, parent) -> TXFConfig.getScreen(parent, MODID));
+TXFConfig.init(MODID, ModConfig.class);
 ```
 
 forked from <a href="https://github.com/TeamMidnightDust/MidnightLib" target="_blank">TeamMidnightDust/MidnightLib</a>
--- forge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
+++ forge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
@@ -1,10 +1,25 @@
 package com.jahirtrap.configlib;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod(ConfigLibMod.MODID)
+@Mod.EventBusSubscriber(modid = ConfigLibMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ConfigLibMod {
 
     public static final String MODID = "configlibtxf";
 
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        ModList.get().forEachModContainer((modid, container) -> {
+            if (TXFConfig.configClass.containsKey(modid)) {
+                container.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
+                        new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfigClient.getScreen(parent, modid)));
+            }
+        });
+    }
 }
