b4ec01616ad2e5b4d036e60e5a0b584f69d1f76e
jahirxtrap <jahirroman2002@gmail.com>
2024-08-29 01:03:56 -0500

    Updates

--- forge/src/main/java/com/jahirtrap/configlib/TXFConfig.java
+++ forge/src/main/java/com/jahirtrap/configlib/TXFConfig.java
@@ -4,9 +4,11 @@ import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLPaths;
 
+import javax.swing.*;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -17,7 +19,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
-@SuppressWarnings("unchecked")
 public abstract class TXFConfig {
     public static class EntryInit {
         boolean centered;
@@ -27,7 +28,11 @@ public abstract class TXFConfig {
     public static final Map<String, Class<? extends TXFConfig>> configClass = new HashMap<>();
     public static Path path;
 
-    public static final Gson gson = new GsonBuilder().excludeFieldsWithModifiers(Modifier.TRANSIENT).excludeFieldsWithModifiers(Modifier.PRIVATE).addSerializationExclusionStrategy(new HiddenAnnotationExclusionStrategy()).setPrettyPrinting().create();
+    public static final Gson gson = new GsonBuilder()
+            .excludeFieldsWithModifiers(Modifier.TRANSIENT).excludeFieldsWithModifiers(Modifier.PRIVATE)
+            .addSerializationExclusionStrategy(new HiddenAnnotationExclusionStrategy())
+            .registerTypeAdapter(ResourceLocation.class, new ResourceLocation.Serializer())
+            .setPrettyPrinting().create();
 
     public static void init(String modid, Class<? extends TXFConfig> config) {
         path = FMLPaths.CONFIGDIR.get().resolve(modid + ".json");
@@ -37,8 +42,7 @@ public abstract class TXFConfig {
             EntryInit init = new EntryInit();
             if (field.isAnnotationPresent(Comment.class)) init.centered = field.getAnnotation(Comment.class).centered();
             if (field.isAnnotationPresent(Entry.class))
-                try {
-                    init.defaultValue = field.get(null);
+                try { init.defaultValue = field.get(null);
                 } catch (IllegalAccessException ignored) {}
             if ((field.isAnnotationPresent(Entry.class) || field.isAnnotationPresent(Comment.class)) && !field.isAnnotationPresent(Server.class) && !field.isAnnotationPresent(Hidden.class) && (FMLEnvironment.dist.isClient()))
                 TXFConfigClient.initClient(modid, field, init);
@@ -55,20 +59,20 @@ public abstract class TXFConfig {
     }
 
     public void writeChanges(String modid) {
-        path = FMLPaths.CONFIGDIR.get().resolve(modid + ".json");
-        try {
-            if (!Files.exists(path)) Files.createFile(path);
+        try { if (!Files.exists(path = FMLPaths.CONFIGDIR.get().resolve(modid + ".json"))) Files.createFile(path);
             Files.write(path, gson.toJson(getClass(modid)).getBytes());
-        } catch (Exception e) {
-            e.fillInStackTrace();
-        }
+        } catch (Exception e) { e.fillInStackTrace(); }
     }
 
     @Retention(RetentionPolicy.RUNTIME) @Target(ElementType.FIELD) public @interface Entry {
-        int width() default 100;
+        int width() default 400;
         double min() default Double.MIN_NORMAL;
         double max() default Double.MAX_VALUE;
         String name() default "";
+        int selectionMode() default -1;        // -1 for none, 0 for file, 1 for directory, 2 for both
+        int fileChooserType() default JFileChooser.OPEN_DIALOG;
+        String[] fileExtensions() default {"*"};
+        int idMode() default -1;               // -1 for none, 0 for item, 1 for block
         boolean isColor() default false;
         boolean isSlider() default false;
         int precision() default 100;
@@ -84,8 +88,6 @@ public abstract class TXFConfig {
 
     public static class HiddenAnnotationExclusionStrategy implements ExclusionStrategy {
         public boolean shouldSkipClass(Class<?> clazz) { return false; }
-        public boolean shouldSkipField(FieldAttributes fieldAttributes) {
-            return fieldAttributes.getAnnotation(Entry.class) == null;
-        }
+        public boolean shouldSkipField(FieldAttributes fieldAttributes) { return fieldAttributes.getAnnotation(Entry.class) == null; }
     }
 }
