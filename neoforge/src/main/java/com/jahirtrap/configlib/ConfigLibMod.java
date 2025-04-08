ed2ccf43a60644e121dd965a984966d0daf06eeb
jahirxtrap <jahirroman2002@gmail.com>
2025-04-08 08:42:13 -0500

    v7.2.8

--- neoforge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
+++ neoforge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
@@ -8,17 +8,12 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-import javax.swing.*;
-
-import static net.minecraft.client.Minecraft.ON_OSX;
-
 @Mod("configlibtxf")
 @EventBusSubscriber(modid = "configlibtxf", bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ConfigLibMod {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         System.setProperty("java.awt.headless", "false");
-        try { if (!ON_OSX) { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }} catch (Exception ignored) {}
         ModList.get().forEachModContainer((modid, container) -> {
             if (TXFConfig.configClass.containsKey(modid)) {
                 container.registerExtensionPoint(IConfigScreenFactory.class, (client, parent) -> TXFConfig.getScreen(parent, modid));
