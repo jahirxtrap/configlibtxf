f4bd565824ba693304536a47536f6f419c8e8c85
jahirxtrap <jahirroman2002@gmail.com>
2025-04-08 08:43:15 -0500

    v10.2.8

--- forge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
+++ forge/src/main/java/com/jahirtrap/configlib/ConfigLibMod.java
@@ -7,17 +7,12 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-import javax.swing.*;
-
-import static net.minecraft.client.Minecraft.ON_OSX;
-
 @Mod("configlibtxf")
 @Mod.EventBusSubscriber(modid = "configlibtxf", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ConfigLibMod {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         System.setProperty("java.awt.headless", "false");
-        try { if (!ON_OSX) { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }} catch (Exception ignored) {}
         ModList.get().forEachModContainer((modid, container) -> {
             if (TXFConfig.configClass.containsKey(modid)) {
                 container.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
