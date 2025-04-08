664c80e19b29dad9cef5541668715820d6be615d
jahirxtrap <jahirroman2002@gmail.com>
2025-04-08 08:41:29 -0500

    v3.2.8

--- fabric/src/main/java/com/jahirtrap/configlib/AutoModMenu.java
+++ fabric/src/main/java/com/jahirtrap/configlib/AutoModMenu.java
@@ -3,17 +3,13 @@ package com.jahirtrap.configlib;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
-import javax.swing.*;
 import java.util.HashMap;
 import java.util.Map;
-
-import static net.minecraft.client.Minecraft.ON_OSX;
 
 public class AutoModMenu implements ModMenuApi {
     @Override
     public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
         System.setProperty("java.awt.headless", "false");
-        try { if (!ON_OSX) { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }} catch (Exception ignored) {}
         HashMap<String, ConfigScreenFactory<?>> map = new HashMap<>();
         TXFConfig.configClass.forEach((modid, cClass) -> map.put(modid, parent -> TXFConfig.getScreen(parent, modid)));
         return map;
